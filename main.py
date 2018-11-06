bd2cb7d3454610353383a81182d0e6ab56c7e69d
Grega Vrbančič <grega.vrbancic@gmail.com>
2018-11-06 09:09:53 +0100

    fix filling up dropdowns

--- main.py
+++ main.py
@@ -6,9 +6,13 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, uic, QtWidgets
+import sys, inspect, re
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import QTimer
 from NiaPy.algorithms.basic import *
 from NiaPy.benchmarks import *
+
 #from Settings import _Settings
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType('ui/main.ui')
@@ -16,15 +20,20 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType('ui/main.ui')
 ALGORITHMS = []
 BENCHMARKS = []
 
-for item in dir():
-    if "Algorithm" in item:
-        ALGORITHMS.append(item)
+for item in inspect.getmembers(sys.modules['NiaPy.algorithms.basic'], inspect.isclass):
+    ALGORITHMS.append(re.sub(r'\B([A-Z])', r' \1', item[0]))
+
+for item in inspect.getmembers(sys.modules['NiaPy.algorithms.modified'], inspect.isclass):
+    ALGORITHMS.append(re.sub(r'\B([A-Z])', r' \1', item[0]))
+
+for item in inspect.getmembers(sys.modules['NiaPy.benchmarks'], inspect.isclass):
+    BENCHMARKS.append(re.sub(r'\B([A-Z])', r' \1', item[0]))
 
 
-class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+class NiaPyGUI(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
+        QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         for algorithm in ALGORITHMS:
@@ -33,9 +42,14 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         for benchmark in BENCHMARKS:
             self.comboBoxBenchmarks.addItem(benchmark)
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    window = MyApp()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = NiaPyGUI()
     window.show()
+
+    # enable killing the app with ctrl-c keypress
+    timer = QTimer()
+    timer.timeout.connect(lambda: None)
+    timer.start(100)
+
     sys.exit(app.exec_())
