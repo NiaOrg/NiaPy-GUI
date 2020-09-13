5686207a9a873c516a4ccc2f7d03a79e4877d526
Francisco J. Solis-Munoz <siscomagma@gmail.com>
2020-09-12 23:46:24 -0500

    Updated for NiaPy 2.0, dynamically added Benchmarks and Algorithms

--- NiaPyGUI/NiaPyGUI.py
+++ NiaPyGUI/NiaPyGUI.py
@@ -1,84 +1,77 @@
 # -*- coding: utf-8 -*-
+import importlib
+import io
+from PIL import Image, ImageChops
+import matplotlib.pyplot as plt
+import NiaPy
+from NiaPy.task.task import StoppingTask
+from NiaPy.benchmarks import *
+from NiaPy.algorithms.modified import *
+from NiaPy.algorithms.basic import *
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
 import matplotlib
 matplotlib.use('Agg')
 
-from PyQt5 import QtCore, QtGui, uic, QtWidgets
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import pyqtSlot
-from NiaPy.algorithms.basic import *
-from NiaPy.algorithms.modified import *
-from NiaPy.benchmarks import *
-import matplotlib.pyplot as plt
-from PIL import Image, ImageChops
-import io
-import importlib
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType('ui/main.ui')
 
-ALGORITHMS = []
-BENCHMARKS = sorted(['Rastrigin', 'Rosenbrock', 'Griewank', 'ExpandedGriewankPlusRosenbrock', 'Sphere', 'Ackley', 'Schwefel', 'Schwefel221', 'Schwefel222', 'ModifiedSchwefel', 'Whitley', 'Alpine1', 'Alpine2', 'HappyCat', 'Ridge', 'ChungReynolds', 'Csendes', 'Pinter','Qing', 'Quintic', 'Salomon', 'SchumerSteiglitz', 'Step', 'Step2','Step3', 'Stepint', 'SumSquares', 'StyblinskiTang', 'BentCigar', 'Weierstrass', 'HGBat', 'Katsuura','Elliptic','Discus','Michalewichz','Levy','Sphere','Sphere2','Sphere3','Trid','Perm','Zakharov','DixonPrice','Powell','CosineMixture','ExpandedSchaffer','SchafferN2','SchafferN4'])
-    
-for item in dir():
-    if "Algorithm" in item:
-        ALGORITHMS.append(item)
-        
+ALGOCLASSES = NiaPy.algorithms.Algorithm.__subclasses__()
+BENCHCLASSES = NiaPy.benchmarks.benchmark.Benchmark.__subclasses__()
+
 def show_benchmark_function(latex_code):
-        buf = io.BytesIO()
-        plt.rc('text', usetex=True)
-        plt.rc('font', family='serif')
-        plt.axis('off')
-        plt.text(0.05, 0.5, latex_code, size=10)
-        plt.savefig(buf, format='png')
-        plt.close()
-        
-        back = (255, 255, 255, 255)
-        
-        im = Image.open(buf)
-        bg = Image.new(im.mode, im.size, back)
-        diff = ImageChops.difference(im, bg)
-        diff = ImageChops.add(diff, diff, 2.0, -100)
-        bbox = diff.getbbox()
-        return im.crop(bbox)
-    
+    buf = io.BytesIO()
+    plt.rc('text.latex', preamble = r'\usepackage{amsmath}')
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.axis('off')
+    plt.text(0.05, 0.5, latex_code, size=10)
+    plt.savefig(buf, format='png')
+    plt.close()
+
+    back = (255, 255, 255, 255)
+
+    im = Image.open(buf)
+    bg = Image.new(im.mode, im.size, back)
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    return im.crop(bbox)
+
+
 class NiaPyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        for algorithm in ALGORITHMS:
-            self.comboBoxAlgorithms.addItem(algorithm)
-
-        for benchmark in BENCHMARKS:
-            self.comboBoxBenchmarks.addItem(benchmark)
-        
+        for algo in ALGOCLASSES:
+            self.comboBoxAlgorithms.addItem(algo.Name[0] + ' (' + algo.Name[1] + ')', algo)
+        for bench in BENCHCLASSES:
+            self.comboBoxBenchmarks.addItem(bench.Name[0], bench)
         self.pushButton.clicked.connect(self.run_algorithm)
-        
-	self.comboBoxBenchmarks.currentIndexChanged.connect(self.selectionchange)
-    
-    def selectionchange(self,i):
-        if self.comboBoxBenchmarks.currentText() == "Ackley":
-            ackley = Ackley.latex_code().replace("\n", "")
-            show_benchmark_function(ackley).save("benchmark_function.png")
-            self.label.setPixmap(QtGui.QPixmap("benchmark_function.png"))
-        elif self.comboBoxBenchmarks.currentText() == "Alpine1":
-            alpine = Alpine1.latex_code().replace("\n", "")
-            show_benchmark_function(alpine).save("benchmark_function.png")
-            self.label.setPixmap(QtGui.QPixmap("benchmark_function.png"))
+        self.comboBoxBenchmarks.currentIndexChanged.connect(self.selection_change)
+
+    def selection_change(self, i):
+        bench = self.comboBoxBenchmarks.itemData(
+            self.comboBoxBenchmarks.currentIndex())
+        show_benchmark_function(bench.latex_code()).save("benchmark_function.png")
+        self.label.setPixmap(QtGui.QPixmap("benchmark_function.png"))
 
     @pyqtSlot()
     def run_algorithm(self):
-        algorithm = str(self.comboBoxAlgorithms.currentText()) 
-        if algorithm == "FlowerPollinationAlgorithm":
-            Algorithm = FlowerPollinationAlgorithm(NP=10, D=20, nFES=1000, p=0.5, benchmark=Ackley())
-            Best = Algorithm.run()
-            self.label_4.setText(str(Best))
-        elif algorithm == "FireflyAlgorithm":
-            Algorithm = FireflyAlgorithm(D=10, NP=20, nFES=1000, alpha=0.5, betamin=0.2, gamma=1.0, benchmark=Sphere())
-            Best = Algorithm.run()
-            self.label_4.setText(str(Best))
-            
+        algo = self.comboBoxAlgorithms.itemData(
+            self.comboBoxAlgorithms.currentIndex())
+        bench = self.comboBoxBenchmarks.itemData(
+            self.comboBoxBenchmarks.currentIndex())
+        task = StoppingTask(D=10, nFES=1000, benchmark=bench())
+        best = algo().run(task=task)
+        self.label_4.setText(str(best))
+
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
